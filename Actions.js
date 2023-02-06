03b3e7e5ab6cfe959c10d5fa52477f43a34aac83
Abdullah <abdullahkhrief@gmail.com>
2023-02-06 16:43:01 +0300

    Have added the style for the menu

---
+++ Actions.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("navbar").style.width = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("navbar").style.width = "0";
+  }
