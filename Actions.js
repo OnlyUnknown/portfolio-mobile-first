fa5e042d6b3d8798df05b0d68725d9860a96eb19
Abdullah <abdullahkhrief@gmail.com>
2023-02-06 23:49:09 +0300

    Added linters for javascript

--- Actions.js
+++ Actions.js
@@ -1,28 +1,17 @@
 function myFunction() {
-    var x = document.getElementById("nav-links");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-
-    
+  const x = document.getElementById('nav-links');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
   }
-
-  function changeIcon(anchor) {
-    var icon = anchor.querySelector("i");
-    icon.classList.toggle('fa-bars');
-    icon.classList.toggle('fa-close');
-  
-     anchor.querySelector("span").textContent = icon.classList.contains('fa-bars');
-  }
-
-
-function statment(name) {
-    var ac = name.querySelector(a)
-    if(document.getElementById("nav-links").style.display === "none"){
-        document.getElementById("btn").classList.toggle('fa-bars')
-    } else{
-        document.getElementById("btn").classList.add('fa-bars')
-    }
 }
+
+function changeIcon() {
+  const icon = document.querySelector('i');
+  icon.classList.toggle('fa-bars');
+  icon.classList.toggle('fa-close');
+}
+
+document.getElementById('nav-links').addEventListener('click', myFunction);
+document.getElementById('btn').addEventListener('click', changeIcon());
