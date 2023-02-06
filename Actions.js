72b2399cc37d42ff736731f41603777e7639a7dc
Abdullah <abdullahkhrief@gmail.com>
2023-02-06 23:19:50 +0300

    Menu icon changing

--- Actions.js
+++ Actions.js
@@ -5,5 +5,24 @@ function myFunction() {
     } else {
       x.style.display = "block";
     }
+
+    
   }
 
+  function changeIcon(anchor) {
+    var icon = anchor.querySelector("i");
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-close');
+  
+     anchor.querySelector("span").textContent = icon.classList.contains('fa-bars');
+  }
+
+
+function statment(name) {
+    var ac = name.querySelector(a)
+    if(document.getElementById("nav-links").style.display === "none"){
+        document.getElementById("btn").classList.toggle('fa-bars')
+    } else{
+        document.getElementById("btn").classList.add('fa-bars')
+    }
+}
