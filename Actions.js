32f24ff04226cf7d622f9c83dec50bba951d68e1
Abdullah <abdullahkhrief@gmail.com>
2023-02-08 13:26:21 +0300

    Have added the ability to see more details about the project

--- Actions.js
+++ Actions.js
@@ -17,7 +17,7 @@ document.getElementById('nav-links').addEventListener('click', myFunction);
 document.getElementById('btn').addEventListener('click', changeIcon());
 
 var seeproject = document.querySelector(".popup-d");
-var seebtn = document.querySelectorAll("*");
+var seebtn = document.querySelector("#myBtn");
 var closebtn = document.querySelector(".close-background");
 
 seebtn.onclick = function() {
@@ -27,4 +27,18 @@ seebtn.onclick = function() {
 
 closebtn.onclick = function() {
   seeproject.style.display = "none"
+};
+
+
+var seeprojectm = document.querySelector(".pop-mobile-container");
+var seebtnm = document.querySelector("#myBtn");
+var closebtnm = document.querySelector(".pop-mobile-close");
+
+seebtnm.onclick = function() {
+  seeprojectm.style.display = "block"
+};
+
+
+closebtn.onclick = function() {
+  seeprojectm.style.display = "none"
 };
