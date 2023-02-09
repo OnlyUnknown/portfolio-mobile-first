39bce84ebc4a40cc43ee770aaf05995f8273acc5
Abdullah <abdullahkhrief@gmail.com>
2023-02-09 13:36:12 +0300

    Added linters

--- Actions.js
+++ Actions.js
@@ -24,107 +24,15 @@ seebtn.onclick = function aaa() {
   seeproject.style.display = 'block';
 };
 
-
 const closebtnm = document.querySelector('.pop-mobile-close');
 
+// projects.forEach((item, index) => {
+//   // write daynamic list
+//   console.log(item, index);
+// });
 
-
-
-const projects = [{
-  name: 'Multi-Post Stories Gain+Glory',
-  description: 'Lorem Ipsum is simply dummy text of the printing'
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
-  + 'text ever since the 1500s, when an unknown printer took a galley of type and'
-  + 'scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing '
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since "
-  + 'the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-  image: 'Snapshoot Portfolio.png',
-  technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootsrap', 'Terminal', 'Codepen'],
-  link: 'https://onlyunknown.github.io/portfolio-mobile-first',
-  source: 'https://onlyunknown.github.io/portfolio-mobile-first',
-},
-
-{
-  name: 'Multi-Post Stories Gain+Glory',
-  description: 'Lorem Ipsum is simply dummy text of the printing'
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
-  + 'text ever since the 1500s, when an unknown printer took a galley of type and'
-  + 'scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing '
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since "
-  + 'the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-  image: 'Snapshoot Portfolio.png',
-  technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootsrap', 'Terminal', 'Codepen'],
-  link: 'https://onlyunknown.github.io/portfolio-mobile-first',
-  source: 'https://onlyunknown.github.io/portfolio-mobile-first',
-},
-
-{
-  name: 'Multi-Post Stories Gain+Glory',
-  description: 'Lorem Ipsum is simply dummy text of the printing'
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
-  + 'text ever since the 1500s, when an unknown printer took a galley of type and'
-  + 'scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing '
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since "
-  + 'the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-  image: 'Snapshoot Portfolio.png',
-  technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootsrap', 'Terminal', 'Codepen'],
-  link: 'https://onlyunknown.github.io/portfolio-mobile-first',
-  source: 'https://onlyunknown.github.io/portfolio-mobile-first',
-},
-
-{
-  name: 'Multi-Post Stories Gain+Glory',
-  description: 'Lorem Ipsum is simply dummy text of the printing'
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
-  + 'text ever since the 1500s, when an unknown printer took a galley of type and'
-  + 'scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing '
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since "
-  + 'the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-  image: 'Snapshoot Portfolio.png',
-  technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootsrap', 'Terminal', 'Codepen'],
-  link: 'https://onlyunknown.github.io/portfolio-mobile-first',
-  source: 'https://onlyunknown.github.io/portfolio-mobile-first',
-},
-
-{
-  name: 'Multi-Post Stories Gain+Glory',
-  description: 'Lorem Ipsum is simply dummy text of the printing'
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
-  + 'text ever since the 1500s, when an unknown printer took a galley of type and'
-  + 'scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing '
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since "
-  + 'the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-  image: 'Snapshoot Portfolio.png',
-  technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootsrap', 'Terminal', 'Codepen'],
-  link: 'https://onlyunknown.github.io/portfolio-mobile-first',
-  source: 'https://onlyunknown.github.io/portfolio-mobile-first',
-},
-
-{
-  name: 'Multi-Post Stories Gain+Glory',
-  description: 'Lorem Ipsum is simply dummy text of the printing'
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
-  + 'text ever since the 1500s, when an unknown printer took a galley of type and'
-  + 'scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing '
-  + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since "
-  + 'the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-  image: 'Snapshoot Portfolio.png',
-  technologies: ['Codekit', 'GitHub', 'JavaScript', 'Bootsrap', 'Terminal', 'Codepen'],
-  link: 'https://onlyunknown.github.io/portfolio-mobile-first',
-  source: 'https://onlyunknown.github.io/portfolio-mobile-first',
-},
-
-];
-
-projects.forEach((item, index) => {
-  // write daynamic list
-  console.log(item, index);
-});
-
-
-
-  // eslint-disable-next-line no-unused-vars
-  const html = `<section class="popup-d" id="myModal">
+// eslint-disable-next-line no-unused-vars
+const html = `<section class="popup-d" id="myModal">
   <div class="pop-mobile-container">
   <div class="img-pop-holder">
       <img src="Mobile-pop.png">
@@ -153,15 +61,16 @@ projects.forEach((item, index) => {
        unknown printer took a galley of type and scrambled it 
       1960s with the relea</p></div>
       <div class="button-p btn-pop pop-img-p"><button class="trans-btn pop-btn">See live<img class="pop-icon" src="Iconseelive.png"></button><button class="trans-btn pop-btn">See source<img class="pop-icon" src="Vectorgithub.png"></button></div>
-  </div></section>`
- 
+  </div></section>`;
 
-  seebtn.onclick = function aaa() {
-    document.getElementById("pop-1").innerHTML = html
-  };
+seebtn.onclick = function aaa() {
+  document.getElementById('pop-1').innerHTML = html;
+};
 
-  closebtn.onclick = function ddd() {
-    document.getElementById("pop-1").innerHTML = "";
-  };
-  
+closebtn.onclick = function ddd() {
+  document.getElementById('pop-1').innerHTML = '';
+};
 
+closebtnm.onclick = function ddd3() {
+  document.getElementById('pop-1').innerHTML = '';
+};
