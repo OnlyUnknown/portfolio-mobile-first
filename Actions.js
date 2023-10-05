2fba2dffbe37826f0cb17b51591f9aac76c5b1e0
Abdullah <abdullahkhrief@gmail.com>
2023-10-05 13:10:25 +0300

    have added the technology and the pic to the pop up window

--- Actions.js
+++ Actions.js
@@ -20,10 +20,10 @@ const projects = [{
   projectId: 'projectOne',
   name: 'Rescue the Earth page',
   description: 'The home page displays all the main information about the'
-  + 'community as weel as speakers information The about page displays information'
-  + 'about previous community work it has a mobile version too for each page',
+  + ' community as weel as speakers information The about page displays information'
+  + ' about previous community work it has a mobile version too for each page',
   mobileimage: 'Mobile-pop.png',
-  image: 'capstone module 1.png',
+  image: 'capstoneModule1.png',
   technologies: ['HTML', 'CSS', 'JavaScript'],
   link: 'https://onlyunknown.github.io/portfolio-mobile-first',
   source: 'https://onlyunknown.github.io/portfolio-mobile-first',
@@ -201,29 +201,6 @@ function Load() {
 // document.addEventListener('DOMContentLoaded', Load);
 
 const seeproject = document.querySelector('#pop-1');
-function popup() {
-  seeproject.innerHTML = html2;
-}
-
-function sss() {
-  const closebtn = document.querySelector('.close-background');
-  const closebtnm = document.querySelector('.pop-mobile-close');
-  closebtnm.onclick = function h1() {
-    seeproject.innerHTML = '';
-  };
-  closebtn.onclick = function h2() {
-    seeproject.innerHTML = '';
-  };
-}
-
-document.addEventListener('DOMContentLoaded', popup);
-
-document.addEventListener('click', sss);
-
-function s1() {
-  seeproject.innerHTML = '';
-}
-document.addEventListener('DOMContentLoaded', s1);
 
 // const form = document.getElementById('contact');
 // const email = form.element['inp-email']
@@ -250,25 +227,83 @@ validateForm();
 
 // Function to create and display a project popup
 function displayProjectPopup(project) {
+   
+  let html = document.createElement("section")
+  html.classList.add("popup-d")
+  html.innerHTML = `<div class="pop-mobile-container">
+  <div class="img-pop-holder">
+      <img src="Mobile-pop.png">
+      <div class="pop-mobile-close">X</div>
+      </div>
+
+<div class="close-background"><i id="pop-close" class="fa fa-close fa-2x"></i></div>
+<div class="pop-img" ><img class="pop-img" src=${project.image}></div>
+<div class="pop-title-container"><div class="pop-title"><h2>${project.name}</h2> <div class="button-p-p btn-pop-d"><button class="trans-btn pop-btn">See live<img class="pop-icon" src="Iconseelive.png"></button><button class="trans-btn pop-btn">See source<img class="pop-icon" src="Vectorgithub.png"></button></div></div>
+</div>
+   <ul class="pop-prog-flex">
+      
+     </ul>
+     <div class="msg-contaier-pop"><p class="pop-msg">${project.description}</p></div>
+      <div class="button-p btn-pop pop-img-p"><button class="trans-btn pop-btn" href"'https://onlyunknown.github.io/portfolio-mobile-first'">See live<img class="pop-icon" src="Iconseelive.png"></button><button class="trans-btn pop-btn" href="'https://onlyunknown.github.io/portfolio-mobile-first'">See source<img class="pop-icon" src="Vectorgithub.png"></button></div>
+  </div>`
+//   const html = `<section class="popup-d">
+//   <div class="pop-mobile-container">
+//   <div class="img-pop-holder">
+//       <img src="Mobile-pop.png">
+//       <div class="pop-mobile-close">X</div>
+//       </div>
+
+// <div class="close-background"><i id="pop-close" class="fa fa-close fa-2x"></i></div>
+// <div class="pop-img" ><img src="Snapshoot Portfolio.png"></div>
+// <div class="pop-title-container"><div class="pop-title"><h2>Multi-Post Stories Gain+Glory</h2> <div class="button-p-p btn-pop-d"><button class="trans-btn pop-btn">See live<img class="pop-icon" src="Iconseelive.png"></button><button class="trans-btn pop-btn">See source<img class="pop-icon" src="Vectorgithub.png"></button></div></div>
+// </div>
+//    <ul class="pop-prog-flex">
+//       <li class="prog-li-pop">Codekit</li>
+//       <li class="prog-li-pop">GitHub</li>
+//       <li class="prog-li-pop">Javascript</li>
+//       <li class="prog-li-pop popg1">Bootstrap</li>
+//       <li class="prog-li-pop popg1">Terminal</li>
+//       <li class="prog-li-pop popg1">Codepen</li>
+//      </ul>
+//      <div class="msg-contaier-pop"><p class="pop-msg">Lorem Ipsum is simply dummy text of the printing'
+//      + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy"
+//      + 'text ever since the 1500s, when an unknown printer took a galley of type and'
+//      + 'scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing '
+//      + "and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since "
+//      + 'the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+//      image: 'Snapshoot Portfolio.png</p></div>
+//       <div class="button-p btn-pop pop-img-p"><button class="trans-btn pop-btn" href"'https://onlyunknown.github.io/portfolio-mobile-first'">See live<img class="pop-icon" src="Iconseelive.png"></button><button class="trans-btn pop-btn" href="'https://onlyunknown.github.io/portfolio-mobile-first'">See source<img class="pop-icon" src="Vectorgithub.png"></button></div>
+//   </div></section>`;
   // Create a div for the popup overlay
+
   const popupOverlay = document.createElement('div');
   popupOverlay.classList.add('project-popup');
   document.body.appendChild(popupOverlay);
 
   // Create a div for the popup content
-  const popupContent = document.createElement('div');
+  let popupContent = document.createElement('div');
   popupContent.classList.add('project-popup-content');
-  popupOverlay.appendChild(popupContent);
+  popupOverlay.appendChild(html);
 
   // Add close button to the popup content
-  const closeButton = document.createElement('span');
-  closeButton.classList.add('close-button');
-  closeButton.innerHTML = '&times;';
-  closeButton.addEventListener('click', () => {
-      popupOverlay.style.display = 'none';
-  });
-  popupContent.appendChild(closeButton);
-
+  const closeButtons = document.querySelectorAll(".close-background")
+  
+  closeButtons.forEach((closeButton) => {
+    closeButton.addEventListener('click', () => {
+        popupOverlay.style.display = 'none';
+    });
+});
+const techul = document.querySelectorAll(".pop-prog-flex")
+techul.forEach((tech) => {
+project.technologies.forEach((techn => {
+  let techli = document.createElement("li")
+  techli.classList.add("prog-li-pop")
+  techli.innerHTML = techn
+  tech.appendChild(techli)
+  
+    
+  }))
+})
   // Create and populate the project details in the popup content
   const projectTitle = document.createElement('h2');
   projectTitle.textContent = project.name;
@@ -338,8 +373,7 @@ function createProjectItems() {
           <h3>${project.name}</h3>
           <button class="view-button">View Details</button>
       `;
-      projectList.appendChild(projectItem);
-
+    
       // Add click event listener to open the popup
       const viewButton = projectItem.querySelector('.view-button');
       viewButton.addEventListener('click', () => {
